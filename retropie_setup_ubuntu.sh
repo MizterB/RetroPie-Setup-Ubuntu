be67dd090f91b4354967995dbe6e167a707d07cc
movisman <morgan@movisman.co.uk>
2020-02-22 23:23:10 +0000

    Update retropie_setup_ubuntu.sh
    existing functions:
    
    function install_retropie_dependencies
    - remove -no-install-recommends as having this causes a blank screen on boot for nvidia cards
    
    function add_retroarch_shaders
     - add line of code to remove the 'pi' centered shaders directory installed by RetroPie-Setup
     - recreate the shaders directory afresh
     - add chown command at the end of the function to make sure everything under /opt/retropie/configs is owned by $USER
    
    function hide_boot_messages
     - improve logic on sed command so instead of relying on GRUB_CMDLINE_LINUX_DEFAULT having the value "quiet" or "" to make the changes needed, now it will replace whatever exists in the quotes with the intended attributes.
    
    function enable_plymouth_theme
     - remove -no-install-recommends just in case it causes issues.
    
    function hide_openbox_windows
     - remove $(cat $USER_HOME/.bash_profile) just before EOF, as this causes the 'startx' code to appear twice in .bash_profile (once at the top, and once below the gnome-terminal commands)
     - reorder this function so it takes place before enable_autostart_xwindows - this ensures that the gnome-terminal code is run before startx is called
     - changed chown command so it changes ownership to $USER from .config recursively instead of .config/openbox - this resolves an issue where .config/dconf (which gets created when gnome-terminal is first launched) is incorrectly owned by root - this then causes DCONF console errors for the gnome-terminal customisation when .bash_profile is called, resulting in no customisation happening because it cannot write to the file correctly (presumably because it is owned by root). Changing the chown command totally resolves this problem, resulting in fully customised black and green terminal with no bars etc.
    
    function enable_autostart_xwindows
     - change ~/ on several lines to $USER_HOME/ to match other functions
    
    function autostart_openbox_apps
     - changed chown command so it changes ownership to $USER from .config recursively instead of .config/openbox, resolves any weird permissions where root takes ownership.
     - changed chown command to /opt/retropie/configs recursively instead of just changing ownership on autostart.sh, otherwise some of the directories get owned by root again. opt/retropie/configs and all files/folders beneath should be owned by $USER
    
    function install_vulkan
     - remove -no-install-recommends just in case it causes issues
    
    new functions:
    
    added optional functions (not enabled by default) for:
     - installing the latest nvidia driver
     - disabling screen blanking
     - changing the GRUB graphics mode to 1920x1080x32 for nicer splash (if supported)
     - final apt update/upgrade and cleanup unneeded packages (autoremove)
     - fix permissions function which catches any potential snags where folders or files under $USER_HOME may get owned by root.
     - downloading retropie only (you need to comment out executing install_retropie in order to use this)
    
    misc:
    
    - reordered functions to match the order they execute in
    - reordered the execution of functions to resolve a couple of minor issues
    - add titles to all functions to tidy up a bit, add additional descriptions/info

--- retropie_setup_ubuntu.sh
+++ retropie_setup_ubuntu.sh
@@ -6,11 +6,11 @@ USER_HOME=/home/$USER
 PLYMOUTH_THEME=retropie-pacman
 
 RETROPIE_CORE_DEPENDS=(
-    xorg openbox pulseaudio alsa-utils menu libglib2.0-bin python-xdg 
+    xorg openbox pulseaudio alsa-utils menu libglib2.0-bin python-xdg
     at-spi2-core libglib2.0-bin dbus-x11 git dialog unzip xmlstarlet
 )
-RETROPIE_EXTRA_DEPENDS=( 
-    openssh-server xdg-utils unclutter 
+RETROPIE_EXTRA_DEPENDS=(
+    openssh-server xdg-utils unclutter
 )
 
 # Add user to sudoers file and disable password prompt
@@ -24,27 +24,13 @@ function disable_sudo_password() {
 # Install RetroPie dependencies
 function install_retropie_dependencies() {
     echo "--------------------------------------------"
-    echo "- Installing RetroPie dependencies"
+    echo "- Update OS packages and install RetroPie dependencies"
     echo "--------------------------------------------"
     apt-get update && apt-get -y upgrade
-    apt-get -y install --no-install-recommends ${RETROPIE_CORE_DEPENDS[@]} ${RETROPIE_EXTRA_DEPENDS[@]}
+    apt-get -y install ${RETROPIE_CORE_DEPENDS[@]} ${RETROPIE_EXTRA_DEPENDS[@]}
 }
 
-function install_latest_video_drivers() {
-    echo "--------------------------------------------"
-    echo "- Installing the latest video drivers"
-    echo "--------------------------------------------"
-    add-apt-repository -y ppa:ubuntu-x-swat/updates
-    apt-get update && apt-get -y upgrade
-}
-
-function install_vulkan() {
-    echo "--------------------------------------------"
-    echo "- Installing Vulkan"
-    echo "--------------------------------------------"
-    apt-get -y install --no-install-recommends mesa-vulkan-drivers
-}
-
+#Install RetroPie
 function install_retropie() {
     echo "--------------------------------------------"
     echo "- Installing RetroPie"
@@ -57,6 +43,68 @@ function install_retropie() {
     $USER_HOME/RetroPie-Setup/retropie_packages.sh samba
     $USER_HOME/RetroPie-Setup/retropie_packages.sh samba install_shares
     chown -R $USER:$USER $USER_HOME/RetroPie-Setup
+}
+
+# Add Retroarch Shaders from official repository
+function add_retroarch_shaders() {
+    echo "--------------------------------------------"
+    echo "- Remove Pi shaders installed by RetroPie-Setup"
+    echo "- then merge common shaders & GLSL shaders"
+    echo "- into RetroArch (from Libretro repository)"
+    echo "--------------------------------------------"
+    # Cleanup pi shaders installed by RetroPie-Setup
+    rm -rf /opt/retropie/configs/all/retroarch/shaders
+    mkdir -p /opt/retropie/configs/all/retroarch/shaders
+    # Install common shaders from Libretro repository
+    git clone --depth=1 https://github.com/libretro/common-shaders.git /tmp/common-shaders
+    cp -r /tmp/common-shaders/* /opt/retropie/configs/all/retroarch/shaders/
+    rm -rf /tmp/common-shaders
+    # Install GLSL shaders from Libretro repository
+    git clone --depth=1 https://github.com/libretro/glsl-shaders.git /tmp/glsl-shaders
+    cp -r /tmp/glsl-shaders/* /opt/retropie/configs/all/retroarch/shaders/
+    rm -rf /tmp/glsl-shaders
+    # Remove git repository from shader dir
+    rm -rf /opt/retropie/configs/all/retroarch/shaders/.git
+    chown -R $USER:$USER /opt/retropie/configs
+}
+
+# Install latest Intel video drivers
+function install_latest_intel_drivers() {
+    echo "--------------------------------------------"
+    echo "- Installing the latest intel drivers"
+    echo "--------------------------------------------"
+    add-apt-repository -y ppa:ubuntu-x-swat/updates
+    apt-get update && apt-get -y upgrade
+}
+
+# Install MESA Vulkan drivers
+function install_vulkan() {
+    echo "--------------------------------------------"
+    echo "- Installing Vulkan"
+    echo "--------------------------------------------"
+    apt-get -y install mesa-vulkan-drivers
+}
+
+# Hide Boot Messages
+function hide_boot_messages() {
+    echo "--------------------------------------------"
+    echo "- Hiding boot messages"
+    echo "--------------------------------------------"
+    # Hide kernel messages and blinking cursor via GRUB
+    sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT=".*"/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash vt.global_cursor_default=0"/g' /etc/default/grub
+    update-grub
+
+    # Hide fsck messages after Plymouth splash
+    echo 'FRAMEBUFFER=y' > /etc/initramfs-tools/conf.d/splash
+    update-initramfs -u
+
+    # Remove cloud-init to suppress its boot messages
+    apt-get purge cloud-init -y
+    rm -rf /etc/cloud/ /var/lib/cloud/
+
+    # Disable motd
+    touch $USER_HOME/.hushlogin
+    chown $USER:$USER $USER_HOME/.hushlogin
 }
 
 # Configure 'pi' user to autologin
@@ -73,53 +121,12 @@ Type=idle
 EOF
 }
 
-# Start X as soon as autologin is complete
-function enable_autostart_xwindows() {
-    echo "--------------------------------------------"
-    echo "- Enabling autostart of XWindows"
-    echo "--------------------------------------------"
-    
-    # Create a .xsession file to launch OpenBox when startx is called
-    echo 'exec openbox-session' >> ~/.xsession
-    chown $USER:$USER ~/.xsession
-
-    # Add startx to .bash_profile
-    cat << EOF >> ~/.bash_profile
-if [[ -z \$DISPLAY ]] && [[ \$(tty) = /dev/tty1 ]]; then
-    #startx -- -nocursor >/dev/null 2>&1
-    startx -- >/dev/null 2>&1
-fi
-EOF
-    chown $USER:$USER ~/.bash_profile
-}
-
-function hide_boot_messages() {
-    echo "--------------------------------------------"
-    echo "- Hiding boot messages"
-    echo "--------------------------------------------"
-    # Hide kernel messages and blinking cursor via GRUB
-    sed -i -e 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet"/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash vt.global_cursor_default=0"/g' /etc/default/grub
-    sed -i -e 's/GRUB_CMDLINE_LINUX_DEFAULT=""/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash vt.global_cursor_default=0"/g' /etc/default/grub
-    update-grub
-
-    # Hide fsck messages after Plymouth splash
-    echo 'FRAMEBUFFER=y' > /etc/initramfs-tools/conf.d/splash
-    update-initramfs -u
-
-    # Remove cloud-init to supress its boot messages
-    apt-get purge cloud-init -y
-    rm -rf /etc/cloud/ /var/lib/cloud/
-
-    # Disable motd
-    touch $USER_HOME/.hushlogin
-    chown $USER:$USER $USER_HOME/.hushlogin
-}
-
+# Enable Plymouth Splash Screen
 function enable_plymouth_theme() {
     echo "--------------------------------------------"
     echo "- Enabling Plymouth themes"
     echo "--------------------------------------------"
-    apt-get -y install --no-install-recommends plymouth plymouth-themes plymouth-x11
+    apt-get -y install plymouth plymouth-themes plymouth-x11
     rm -rf /tmp/plymouth-themes
     git clone --depth=1 https://github.com/HerbFargus/plymouth-themes.git /tmp/plymouth-themes
     mv /tmp/plymouth-themes/* /usr/share/plymouth/themes/
@@ -128,6 +135,7 @@ function enable_plymouth_theme() {
     update-initramfs -u
 }
 
+# Hide Openbox Windows and reduce visibility of terminal
 function hide_openbox_windows() {
     echo "--------------------------------------------"
     echo "- 'Hiding' OpenBox windows"
@@ -143,7 +151,6 @@ $GNOME_TERMINAL_SETTINGS background-color '#000000'
 $GNOME_TERMINAL_SETTINGS cursor-blink-mode 'off'
 $GNOME_TERMINAL_SETTINGS scrollbar-policy 'never'
 $GNOME_TERMINAL_SETTINGS audible-bell 'false'
-$(cat $USER_HOME/.bash_profile)
 EOF
     chown $USER:$USER $USER_HOME/.bash_profile
 
@@ -162,9 +169,28 @@ EOF
     sed -i '/<applications>/r /tmp/rc.xml.applications' $USER_HOME/.config/openbox/rc.xml
     rm /tmp/rc.xml.applications
     sed -e 's/<keepBorder>yes<\/keepBorder>/<keepBorder>no<\/keepBorder>/g' -i $USER_HOME/.config/openbox/rc.xml
-    chown -R $USER:$USER $USER_HOME/.config/openbox
+    chown -R $USER:$USER $USER_HOME/.config
 }
 
+# Start X as soon as autologin is complete
+function enable_autostart_xwindows() {
+    echo "--------------------------------------------"
+    echo "- Enabling autostart of XWindows"
+    echo "--------------------------------------------"
+    # Create a .xsession file to launch OpenBox when startx is called
+    echo 'exec openbox-session' >> $USER_HOME/.xsession
+    chown $USER:$USER $USER_HOME/.xsession
+
+    # Add startx to .bash_profile
+    cat << EOF >> $USER_HOME/.bash_profile
+if [[ -z \$DISPLAY ]] && [[ \$(tty) = /dev/tty1 ]]; then
+    startx -- >/dev/null 2>&1
+fi
+EOF
+    chown $USER:$USER $USER_HOME/.bash_profile
+}
+
+# Autostart Openbox Applications
 function autostart_openbox_apps() {
     echo "--------------------------------------------"
     echo "- Enabling OpenBox autostart applications and RetroPie autostart.sh"
@@ -172,13 +198,13 @@ function autostart_openbox_apps() {
     # OpenBox autostarts unclutter, then passes off to the RetroPie autostart
     mkdir -p $USER_HOME/.config/openbox
     echo 'unclutter -idle 0.01 -root' >> $USER_HOME/.config/openbox/autostart
-    echo '/opt/retropie/configs/all/autostart.sh' >> $USER_HOME/.config/openbox/autostart 
-    chown -R $USER:$USER $USER_HOME/.config/openbox
+    echo '/opt/retropie/configs/all/autostart.sh' >> $USER_HOME/.config/openbox/autostart
+    chown -R $USER:$USER $USER_HOME/.config
     # Create RetroPie autostart
     mkdir -p /opt/retropie/configs/all
     touch /opt/retropie/configs/all/autostart.sh
     chmod +x /opt/retropie/configs/all/autostart.sh
-    chown $USER:$USER /opt/retropie/configs/all/autostart.sh
+    chown -R $USER:$USER /opt/retropie/configs
     cat << EOF > /opt/retropie/configs/all/autostart.sh
 #! /bin/bash
 
@@ -189,36 +215,96 @@ gnome-terminal --full-screen --hide-menubar -- emulationstation --no-splash
 EOF
 }
 
-function add_retroarch_shaders() {
+# Install Latest Nvidia Drivers
+function install_latest_nvidia_drivers() {
     echo "--------------------------------------------"
-    echo "- Merging common shaders & GLSL shaders"
-    echo "- into to RetroArch"
+    echo "- Install Latest Nvidia Drivers"
     echo "--------------------------------------------"
-    # Common shaders
-    git clone --depth=1 https://github.com/libretro/common-shaders.git /tmp/common-shaders
-    cp -r /tmp/common-shaders/* /opt/retropie/configs/all/retroarch/shaders/
-    rm -rf /tmp/common-shaders
-    # GLSL shaders
-    git clone --depth=1 https://github.com/libretro/glsl-shaders.git /tmp/glsl-shaders
-    cp -r /tmp/glsl-shaders/* /opt/retropie/configs/all/retroarch/shaders/
-    rm -rf /tmp/glsl-shaders
-    # Remove git repository from shader dir
-    rm -rf /opt/retropie/configs/all/retroarch/shaders/.git
+    apt-get -y install ubuntu-drivers-common
+    add-apt-repository -y ppa:graphics-drivers/ppa
+    ubuntu-drivers autoinstall
+}
+
+# Disable screen blanking (only happens outside of EmulationStation)
+function disable_screen_blanking() {
+    echo "--------------------------------------------"
+    echo "- Disable screen blanking (only happens outside of EmulationStation)"
+    echo "- Stops the display from doing any ‘screen blanking’"
+    echo "- after a short period of activity"
+    echo "--------------------------------------------"
+    sed -i '1 i\xset s off && xset -dpms' $USER_HOME/.xsession
+}
+
+# Change GRUB Graphics Mode to higher resolution
+# Beware using this function!
+function change_grub_gfxmode() {
+    echo "--------------------------------------------"
+    echo "- Change GRUB Graphics Mode from default"
+    echo "- Only use if supported!"
+    echo "- Run vbeinfo or videoinfo from GRUB"
+    echo "- command line to see supported modes"
+    echo "- Change the resolution to one which is supported"
+    echo "--------------------------------------------"
+    sed -i 's/#GRUB_GFXMODE=.*/GRUB_GFXMODE=1920x1080x32/g' /etc/default/grub
+    update-grub
+}
+
+# Fix permissions function recursively on $USER_HOME directory
+function fix_home_permissions() {
+    echo "--------------------------------------------"
+    echo "- Fix any snags relating to file/folder permissions underneath $USER_HOME"
+    echo "- Change owner to $USER on all files and directories under $USER_HOME"
+    echo "--------------------------------------------"
+    chown -R $USER:$USER $USER_HOME/
+}
+
+# Cleanup unneeded packages
+function cleanup_unneeded_packages() {
+    echo "--------------------------------------------"
+    echo "- Final update and cleanup unneeded packages"
+    echo "--------------------------------------------"
+    apt-get update && apt-get -y upgrade
+    apt-get -y autoremove
+}
+
+# Download RetroPie only - do not install
+function download_retropie_only() {
+    echo "--------------------------------------------"
+    echo "- Downloading RetroPie"
+    echo "--------------------------------------------"
+    # Get Retropie Setup script
+    cd $USER_HOME
+    git clone --depth=1 https://github.com/RetroPie/RetroPie-Setup.git
+    chown -R $USER:$USER $USER_HOME/RetroPie-Setup
 }
 
 # Force this script to run as root
 [ `whoami` = root ] || { sudo "$0" "$@"; exit $?; }
 
-# Execute these steps
+# Execute OS Steps and dependencies
 disable_sudo_password
 install_retropie_dependencies
 install_retropie
-hide_boot_messages
-enable_autologin_tty
-enable_autostart_xwindows
-enable_plymouth_theme
-hide_openbox_windows
-autostart_openbox_apps
-install_latest_video_drivers
+install_latest_intel_drivers
 install_vulkan
 add_retroarch_shaders
+hide_boot_messages
+enable_autologin_tty
+enable_plymouth_theme
+hide_openbox_windows
+enable_autostart_xwindows
+autostart_openbox_apps
+
+# Optional steps (uncomment as needed)
+install_latest_nvidia_drivers
+disable_screen_blanking
+change_grub_gfxmode
+
+# Final cleanup
+fix_home_permissions
+cleanup_unneeded_packages
+
+# retropie download only, do not install it
+# comment out the install_retropie function above and uncomment the below function to use this
+# useful if you want to install retropie components yourself rather than run a basic install
+#download_retropie_only
